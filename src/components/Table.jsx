32ec9aa33ceae7d7708cf5bd966f04f6b9d1283f
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-03-28 16:15:47 -0300

    REQ2 - Finalizado, Filtro para o nome criado

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,7 @@ import { planetsData } from '../helpers/data';
 
 class Table extends Component {
   state = {
+    filter: '',
     results: [],
     titles: [],
   };
@@ -14,32 +15,50 @@ class Table extends Component {
   filterData = async () => {
     const data = await planetsData();
     const { results } = data;
+    const { filter } = this.state;
     results.forEach((e) => delete e.residents);
+    const filtered = results.filter((e) => (e.name.toUpperCase())
+      .includes(filter.toUpperCase()));
     const titles = Object.keys(results[0]);
-    this.setState({ results, titles });
+    this.setState({ results: filtered, titles });
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    }, this.filterData);
   };
 
   render() {
     const { results, titles } = this.state;
     return (
-      <table>
-        <thead>
-          <tr>
-            {titles.map((e) => (
-              <th key={ `${e} key1` }>{e}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-
-          {results.map((e) => (
-            <tr key={ `${e.edited} key2` }>
-              {titles.map((key) => <td key={ `${key} key 3` }>{e[key]}</td>)}
+      <>
+        <input
+          type="text"
+          name="filter"
+          onChange={ this.handleChange }
+          data-testid="name-filter"
+        />
+        <table>
+          <thead>
+            <tr>
+              {titles.map((e) => (
+                <th key={ `${e} key1` }>{e}</th>
+              ))}
             </tr>
-          ))}
-        </tbody>
+          </thead>
+          <tbody>
 
-      </table>
+            {results.map((e) => (
+              <tr key={ `${e.edited} key2` }>
+                {titles.map((key) => <td key={ `${key} key 3` }>{e[key]}</td>)}
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </>
     );
   }
 }
