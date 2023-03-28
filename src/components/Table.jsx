a1c98549bc7301759edb53cdd30f85fbc970259e
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-03-28 18:08:54 -0300

    REQ5 - FInalizado, testes feitos

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,8 +4,13 @@ import { planetsData } from '../helpers/data';
 class Table extends Component {
   state = {
     filter: '',
+    value: 0,
+    comparasion: 'maior que',
+    column: 'population',
     results: [],
     titles: [],
+    filterBool: false,
+    resultsImutable: [],
   };
 
   componentDidMount() {
@@ -15,31 +20,108 @@ class Table extends Component {
   filterData = async () => {
     const data = await planetsData();
     const { results } = data;
-    const { filter } = this.state;
     results.forEach((e) => delete e.residents);
-    const filtered = results.filter((e) => (e.name.toUpperCase())
-      .includes(filter.toUpperCase()));
     const titles = Object.keys(results[0]);
-    this.setState({ results: filtered, titles });
+    this.setState({ results, titles, resultsImutable: results });
+  };
+
+  filterName = async () => {
+    const { filter, resultsImutable } = this.state;
+    const filtered = resultsImutable.filter((e) => (e.name.toUpperCase())
+      .includes(filter.toUpperCase()));
+    this.setState({ results: filtered });
   };
 
   handleChange = ({ target }) => {
     const { value, name } = target;
     this.setState({
       [name]: value,
-    }, this.filterData);
+    }, this.filterName);
+  };
+
+  handleClick = () => {
+    const { value, comparasion, column, resultsImutable } = this.state;
+
+    if (comparasion === 'maior que') {
+      const filtered = resultsImutable.filter((e) => {
+        const verify = Number(e[column]) > Number(value);
+        return verify;
+      });
+      this.setState({ resultsImutable: filtered, filterBool: true });
+    }
+
+    if (comparasion === 'menor que') {
+      const filtered = resultsImutable.filter((e) => {
+        const verify = Number(e[column]) < Number(value);
+        return verify;
+      });
+      this.setState({ resultsImutable: filtered, filterBool: true });
+    }
+
+    if (comparasion === 'igual a') {
+      const filtered = resultsImutable.filter((e) => {
+        const verify = Number(e[column]) === Number(value);
+        return verify;
+      });
+      this.setState({ resultsImutable: filtered, filterBool: true });
+    }
   };
 
   render() {
-    const { results, titles } = this.state;
+    const { results, titles, value, filterBool, resultsImutable } = this.state;
     return (
       <>
+        <label htmlFor="">
+          Coluna
+          <select
+            data-testid="column-filter"
+            name="column"
+            onChange={ this.handleChange }
+          >
+            <option>population</option>
+            <option>orbital_period</option>
+            <option>diameter</option>
+            <option>rotation_period</option>
+            <option>surface_water</option>
+          </select>
+        </label>
+
+        <label htmlFor="">
+          Operador
+          <select
+            onChange={ this.handleChange }
+            data-testid="comparison-filter"
+            name="comparasion"
+          >
+            <option>maior que</option>
+            <option>menor que</option>
+            <option>igual a</option>
+          </select>
+        </label>
+        <label>
+          Value
+          <input
+            type="number"
+            name="value"
+            value={ value }
+            data-testid="value-filter"
+            onChange={ this.handleChange }
+          />
+        </label>
+
         <input
           type="text"
           name="filter"
           onChange={ this.handleChange }
           data-testid="name-filter"
         />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ this.handleClick }
+        >
+          Filter
+        </button>
         <table>
           <thead>
             <tr>
@@ -50,7 +132,11 @@ class Table extends Component {
           </thead>
           <tbody>
 
-            {results.map((e) => (
+            {filterBool ? resultsImutable.map((e) => (
+              <tr key={ `${e.edited} key2` }>
+                {titles.map((key) => <td key={ `${key} key 3` }>{e[key]}</td>)}
+              </tr>
+            )) : results.map((e) => (
               <tr key={ `${e.edited} key2` }>
                 {titles.map((key) => <td key={ `${key} key 3` }>{e[key]}</td>)}
               </tr>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 import App from '../App';
 
-test('I am your test', () => {
+test('Teste app', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const colunaInput = screen.getByText('Coluna');
+  expect(colunaInput).toBeInTheDocument();
+  const table = screen.getByRole('table');
+  expect(table).toBeInTheDocument();
+  const valueInput = screen.getByRole('spinbutton', {
+    name: /value/i
+  });
+  
+  expect(await screen.findAllByRole('row')).toHaveLength(1);
+  expect(valueInput).toBeInTheDocument();
+  const filterButton = screen.getByRole('button', {
+    name: /filter/i
+  });
+  userEvent.type(valueInput, '10000')
+  expect(valueInput).toHaveValue(10000);
+  userEvent.click(filterButton)
+  expect(await screen.findAllByRole('row')).toHaveLength(1);
+  
 });
