1e519b133c4a011c036ad97b2ed5db44585747ec
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-04-08 22:35:38 -0300

    testes adicionados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Table from './components/Table';
+import PlanetsProvider from './context/PlanetsContext';
 
 export default class App extends Component {
   render() {
     return (
-      <Table />
+      <PlanetsProvider>
+        <Table />
+      </PlanetsProvider>
     );
   }
 }
